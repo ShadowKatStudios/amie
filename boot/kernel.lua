68d3e763853599a866606ea240bc3c292f42d3a1
shadowkat10 <redtailedairways@gmail.com>
2015-04-16 20:51:54 +1000

    Some attempts at making a VFS. It didn't work. At least the main part works, I guess. Shelving for now.

--- boot/kernel.lua
+++ boot/kernel.lua
@@ -12,6 +12,8 @@ function syscall(name,...)
  end
 end
 
+component.proxy(computer.getBootAddress()).remove("/system.log") -- clean off the old system log
+
 syscall("register","log", function(data)
  local bootfs=component.proxy(computer.getBootAddress())
  local handle=bootfs.open("system.log","a")
@@ -86,7 +88,7 @@ end)
 syscall("log","Term I/O initiated")
 
 -- testing, thanks stackoverflow
-
+-- gonna leave this in, methinks. Super-useful function.
 function string.split(inputstr, sep)
  if sep == nil then
   sep = "%s"
@@ -103,6 +105,18 @@ end
 fs = {}
 fs.drive_map={}
 fs.drive_map["boot"]=component.proxy(computer.getBootAddress())
+fs.drive_map["vfs"]={}
+function fs.drive_map.vfs.list()
+ syscall("log","VFS list called")
+ local t = {}
+ for k,v in pairs(fs.drive_map) do
+  table.insert(t,k)
+  syscall("log","VFS list magic: "..v)
+ end
+ t[n]=#t
+ --return t
+ return { "Fuck it, I don't need a fancy meta-VFS to make everything else, it's just stopping progress." }
+end
 
 syscall("register","fs_resolve",function(path)
  syscall("log","Splitting path:")
@@ -110,14 +124,41 @@ syscall("register","fs_resolve",function(path)
  for k,v in pairs(fields) do
   syscall("log",v)
  end
- drive=table.remove(fields,1)
+ local drive=table.remove(fields,1)
+ if drive == nil or drive == "" then
+  drive = "vfs"
+ end
  dpath = ""
  for k,v in pairs(fields) do
   dpath = dpath .. "/" .. v
  end
+ if dpath == "" then
+  dpath = "/"
+ end
  return drive,dpath
 end)
+
+-- poptarts get
+
+syscall("register","fs_exec_on_drive",function(drive,method,...)
+ return fs.drive_map[drive][method](...)
+end)
+
+--sanity check, we could probably use one or two
 local testpath = "/boot/maybe/init.lua"
 local a,b=syscall("fs_resolve",testpath)
 syscall("log","FS resolver init, sanity check: "..testpath.." resolves to drive ".. a .. " with path ".. b)
+-- fs_exec_on_drive test
+syscall("log","Attempting to list /boot/ as a sanity check:")
+local t = syscall("fs_exec_on_drive","boot","list","/")
+for k,v in pairs(t) do
+ syscall("log",k.." : "..v)
+end
+-- this is a bad idea, VFS listing, prepare for everything to crash
+-- no crash, just shit all happening. Good to know.
+syscall("log","Attempting to list / as a sanity check:")
+local t = syscall("fs_exec_on_drive","vfs","list","/")
+for k,v in pairs(t) do
+ syscall("log",k.." : "..v)
+end
 
