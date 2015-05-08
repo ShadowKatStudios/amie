768fabd23302918a04b5500188bb47e738b1680f
shadowkat10 <redtailedairways@gmail.com>
2015-05-08 18:28:17 +1000

    Re-wrote terminal handler, now it's a simple tty handler like it should be. Keyboard coming soon.

--- system/init/sttyh.lua
+++ system/init/sttyh.lua
@@ -1,57 +1,35 @@
 if component.list("gpu")() ~= nil and component.list("screen")() ~= nil then
  -- screen init
  local ttytab = {}
- ttytab.y = 1
  ttytab.gpu = {}
  ttytab.w,ttytab.h=0,0
- ttytab.cstr = ""
- ttytab.stack={}
+ ttytab.cx,ttytab.cy = 1,1 -- current x and y
  local function gpu_init()
   ttytab.gpu=component.proxy(component.list("gpu")())
   ttytab.gpu.bind(component.list("screen")())
   ttytab.w, ttytab.h = ttytab.gpu.getResolution()
   ttytab.gpu.setResolution(w,h)
-  ttytab.gpu.setBackground(0x000000)
-  ttytab.gpu.setForeground(0xFFFFFF)
-  ttytab.gpu.fill(1,1,w,h," ")  
+  ttytab.gpu.setBackground(0x073642)
+  ttytab.gpu.setForeground(0xFDF6E3)
+  ttytab.gpu.fill(1,1,w,h," ")
+  ttytab.gpu.set(1,1,"█")
  end
- syscall("event_listen","writeln",function(v)
-  if ttytab.gpu and ttytab.screen then
-   ttytab.gpu.set(1, y, msg)
-   if ttytab.y == ttytab.h - 1 then
-    ttytab.gpu.copy(1, 2, w, h - 1, 0, -1)
-    ttytab.gpu.fill(1, h, w, 1, " ")
-   else
-    y = y + 1
+ if ttytab.gpu and ttytab.screen then
+  syscall("event_listen","write",function(v)
+   for char in v:gmatch(".") do
+    if char == "\n" then
+     ttytab.cx,ttytab.cy = 1,ttytab.cy+1 -- go to next line, return to start of line
+    elseif char == "\r" then
+     ttytab.cx = 1 -- return to start of line
+    elseif char == "\b" then
+     if ttytab.cx > 1 then
+      ttytab.cx = ttytab.cx - 1
+      ttytab.gpu.set(ttytab.cx,ttytab.cy,"█ ")
+     end
+    else
+     ttytab.gpu.set(ttytab.cx-1,ttytab,cy,char.."█ ")
+    end
    end
-  end
- end)
- local function redraw_text()
-  local lineend = "_"
-  local processed = cstr
-  for k,v in pairs(1,w) do
-   processed = processed .. " "
-  end
-  processed = processed..lineend
-  ttytab.gpu.set(1,h,processed)
+  end)
  end
- syscall("event_listen","char",function(char)
-  cstr = cstr .. char
-  redraw_text()
- end)
- syscall("event_listen","key_down",function(_,char,code,_)
-  if code == "<backspace>" then
-   cstr = cstr.sub(1,cstr.len()-1)
-   redraw_text()
-  elseif code == "<enter>" then
-   syscall("event_push","readlndone",cstr)
-   cstr=""
-   redraw_text()
-  else
-   cstr = cstr .. string.char(char)
-  end
- end)
- syscall("event_listen","readlndone",function(str)
-  syscall("event_push","readln",str)
- end)
 end
